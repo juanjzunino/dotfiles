9bfe7ed6067229cf33d97e7f1b93ab0c63da7a4e
Juanjo Zunino <juanj.zunino@gmail.com>
2020-08-30 19:42:10 -0300

    Add a bunch of dotfiles

---
+++ brew.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Install Brew Packages
+brew install git
+brew install tree
+brew install wget
+brew install htop
+brew install tldr
+brew install trash
+
+# Install MacOS Applications with Brew Cask
+brew cask install sublime-text
+brew cask install pycharm
+brew cask install datagrip
+brew cask install rstudio
+brew cask install vagrant
+brew cask install virtualbox
+brew cask install notion
+brew cask install obsidian
+brew cask install google-chrome
+brew cask install flux
+brew cask install 1password
+brew cask install whatsapp
+brew cask install spotify
+brew cask install slack
+brew cask install gephi
+brew cask install cleanmymac
