994d57aef1f8b606a7151539f1a45bdb3d8a3e69
Juanjo Zunino <juanj.zunino@gmail.com>
2021-04-29 00:20:51 -0300

    fix go

--- editor/.config/nvim/lua/lsp/go-ls.lua
+++ editor/.config/nvim/lua/lsp/go-ls.lua
@@ -1,3 +1,6 @@
-require'lspconfig'.gopls.setup{
+require'lspconfig'.gopls.setup {
   cmd = {DATA_PATH .. "/lspinstall/go/gopls"},
+  settings = {gopls = {analyses = {unusedparams = true}, staticcheck = true}},
+  root_dir = require'lspconfig'.util.root_pattern(".git", "go.mod", "."),
+  init_options = {usePlaceholders = true, completeUnimported = true}
 }
