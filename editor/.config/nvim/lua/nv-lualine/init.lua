a6e9060e8fd3dbfe03c4db5d9141f11514201502
Juanjo Zunino <juanj.zunino@gmail.com>
2021-05-03 17:56:48 -0300

    lualine settings

--- editor/.config/nvim/lua/nv-lualine/init.lua
+++ editor/.config/nvim/lua/nv-lualine/init.lua
@@ -1,3 +1,25 @@
-require('lualine').setup{
-  theme = 'gruvbox',
+require('lualine').setup {
+  options = {
+    theme = 'gruvbox',
+    section_separators = {'', ''},
+    component_separators = {'', ''},
+    disabled_filetypes = {},
+    icons_enabled = true
+  },
+  sections = {
+    lualine_a = {{'mode', upper = true}},
+    lualine_b = {{'branch', icon = ''}},
+    lualine_c = {{'filename', file_status = true}},
+    lualine_x = {'encoding', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  }
 }
