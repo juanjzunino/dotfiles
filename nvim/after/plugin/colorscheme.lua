003421306adef426bf11d3db39657cb9728434b3
Juanjo Zunino <juanj.zunino@gmail.com>
2021-07-29 17:10:21 -0300

    add colorizer

--- nvim/after/plugin/colorscheme.lua
+++ nvim/after/plugin/colorscheme.lua
@@ -2,6 +2,9 @@
 vim.o.termguicolors = true
 vim.o.background = 'dark'
 
+-- Colorizer
+require'colorizer'.setup()
+
 -- Colorscheme
 local base16 = require 'base16'
 base16(base16.themes["gruvbox-dark-hard"], true)
