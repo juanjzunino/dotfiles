8d5becee362b16a23106e2ddfb159b5b2552b4f1
Juanjo Zunino <juanj.zunino@gmail.com>
2022-01-22 14:38:51 -0300

    update references keybind and add symbols keybind

--- nvim/lua/jj/lspconfig.lua
+++ nvim/lua/jj/lspconfig.lua
@@ -24,13 +24,15 @@ local custom_attach = function(client, bufnr)
   buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
 	buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
 	buf_set_keymap('n', '<space>a', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-	buf_set_keymap('n', 'gh', '<cmd>lua require("telescope.builtin").lsp_references()<CR>', opts)
-	buf_set_keymap('n', 'gr', '<cmd>Trouble lsp_references<cr>', opts)
+	buf_set_keymap('n', 'gr', '<cmd>lua require("telescope.builtin").lsp_references()<CR>', opts)
+	buf_set_keymap('n', 'gh', '<cmd>Trouble lsp_references<cr>', opts)
 	buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
 	buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
   buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
   buf_set_keymap('n', '<space>q', '<cmd>lua require("telescope.builtin").lsp_document_diagnostics()<CR>', opts)
   buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+	buf_set_keymap('n', '<space>wd', '<cmd>lua require("telescope.builtin").lsp_document_symbols()<CR>', opts)
+	buf_set_keymap('n', '<space>ww', '<cmd>lua require("telescope.builtin").lsp_workspace_symbols()<CR>', opts)
 
   -- Autoformat
   if client.resolved_capabilities.document_formatting then
