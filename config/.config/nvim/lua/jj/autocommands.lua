f6d3fe8f4d28e34f11c8ca8032446f068ce2aa94
Juanjo Zunino <juanj.zunino@gmail.com>
2022-07-13 22:33:57 -0300

    update to new api

--- config/.config/nvim/lua/jj/autocommands.lua
+++ config/.config/nvim/lua/jj/autocommands.lua
@@ -1,33 +1,27 @@
-local nv_utils = {}
+local api = vim.api
 
-function nv_utils.define_augroups(definitions)
-	-- Create autocommand groups based on the passed definitions
-	-- The key will be the name of the group, and each definition
-	-- within the group should have:
-	--    1. Trigger
-	--    2. Pattern
-	--    3. Text
-	-- just like how they would normally be defined from Vim itself
-	for group_name, definition in pairs(definitions) do
-		vim.cmd("augroup " .. group_name)
-		vim.cmd("autocmd!")
+-- Highlight on yank
+local yank_acmd = api.nvim_create_augroup("YankHighlight", { clear = true })
+api.nvim_create_autocmd("TextYankPost", {
+	command = "silent! lua vim.highlight.on_yank()",
+	group = yank_acmd,
+})
 
-		for _, def in pairs(definition) do
-			local command = table.concat(vim.tbl_flatten({ "autocmd", def }), " ")
-			vim.cmd(command)
-		end
+-- Format Options
+local buf_win_acmd = api.nvim_create_augroup("buf_win", { clear = true })
+api.nvim_create_autocmd("BufWinEnter", {
+	command = "setlocal formatoptions-=c formatoptions-=r formatoptions-=o",
+	group = buf_win_acmd,
+})
 
-		vim.cmd("augroup END")
-	end
-end
+local buf_read_acmd = api.nvim_create_augroup("bun_win", { clear = true })
+api.nvim_create_autocmd("BufRead", {
+	command = "setlocal formatoptions-=c formatoptions-=r formatoptions-=o",
+	group = buf_read_acmd,
+})
 
-nv_utils.define_augroups({
-	_general_settings = {
-		{ "TextYankPost", "*", "lua require('vim.highlight').on_yank({higroup = 'Search', timeout = 200})" },
-		{ "BufWinEnter", "*", "setlocal formatoptions-=c formatoptions-=r formatoptions-=o" },
-		{ "BufRead", "*", "setlocal formatoptions-=c formatoptions-=r formatoptions-=o" },
-		{ "BufNewFile", "*", "setlocal formatoptions-=c formatoptions-=r formatoptions-=o" },
-		{ "BufRead", "*.md", "set filetype=markdown" },
-		{ "BufRead", "*.tex", "set filetype=tex" },
-	},
+local buf_newfile_acmd = api.nvim_create_augroup("bue_newfile", { clear = true })
+api.nvim_create_autocmd("BufNewFile", {
+	command = "setlocal formatoptions-=c formatoptions-=r formatoptions-=o",
+	group = buf_newfile_acmd,
 })
