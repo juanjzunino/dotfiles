cf5e22b38a91a31dbb24cea72b5401486d7c49f7
Juanjo Zunino <juanj.zunino@gmail.com>
2022-07-26 01:53:19 -0300

    define augroup inside autocommand

--- config/.config/nvim/lua/jj/autocommands.lua
+++ config/.config/nvim/lua/jj/autocommands.lua
@@ -1,27 +1,23 @@
 local api = vim.api
 
 -- Highlight on yank
-local yank_acmd = api.nvim_create_augroup("YankHighlight", { clear = true })
 api.nvim_create_autocmd("TextYankPost", {
 	command = "silent! lua vim.highlight.on_yank()",
-	group = yank_acmd,
+	group = api.nvim_create_augroup("YankHighlight", { clear = true }),
 })
 
 -- Format Options
-local buf_win_acmd = api.nvim_create_augroup("buf_win", { clear = true })
 api.nvim_create_autocmd("BufWinEnter", {
 	command = "setlocal formatoptions-=c formatoptions-=r formatoptions-=o",
-	group = buf_win_acmd,
+	group = api.nvim_create_augroup("buf_win", { clear = true }),
 })
 
-local buf_read_acmd = api.nvim_create_augroup("bun_win", { clear = true })
 api.nvim_create_autocmd("BufRead", {
 	command = "setlocal formatoptions-=c formatoptions-=r formatoptions-=o",
-	group = buf_read_acmd,
+	group = api.nvim_create_augroup("bun_win", { clear = true }),
 })
 
-local buf_newfile_acmd = api.nvim_create_augroup("bue_newfile", { clear = true })
 api.nvim_create_autocmd("BufNewFile", {
 	command = "setlocal formatoptions-=c formatoptions-=r formatoptions-=o",
-	group = buf_newfile_acmd,
+	group = api.nvim_create_augroup("bue_newfile", { clear = true }),
 })
