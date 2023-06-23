ee2cdafe592c8c52337f8496be968ad99f709473
Juanjo Zunino <juanj.zunino@gmail.com>
2023-06-23 11:35:22 -0300

    Default settings for lualine

--- config/.config/nvim/lua/jj/lualine.lua
+++ config/.config/nvim/lua/jj/lualine.lua
@@ -4,43 +4,4 @@ if not status_ok then
 	return
 end
 
-lualine.setup({
-	options = {
-		icons_enabled = true,
-		theme = "vscode",
-		component_separators = { left = '', right = '' },
-		section_separators = { left = '', right = '' },
-		disabled_filetypes = {},
-	},
-	sections = {
-		lualine_a = {
-			{ "mode", upper = true },
-		},
-		lualine_b = {
-			{ "branch" },
-		},
-		lualine_c = {
-			{ "filename",    file_status = true },
-			{ "diagnostics", sources = { "nvim_diagnostic" } },
-		},
-		lualine_x = {
-			"filetype",
-		},
-		lualine_y = {
-			"encoding",
-		},
-		lualine_z = {
-			"location",
-		},
-	},
-	inactive_sections = {
-		lualine_a = { "filename" },
-		lualine_b = {},
-		lualine_c = {},
-		lualine_x = {},
-		lualine_y = {},
-		lualine_z = { "filetype" },
-	},
-	tabline = {},
-	extensions = {},
-})
+lualine.setup({})
